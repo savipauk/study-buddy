afda2c710a41b741f7945d61b041c6fc156c8618
Mila1308 <mpodrug1308@gmail.com>
2024-10-22 21:55:59 +0200

    Initialize backend

---
+++ src/test/java/com/study_buddy/study_buddy/StudyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study_buddy.study_buddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
