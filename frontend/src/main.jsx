d4e96b4d3a7ca5cffcc2bf30aca5fffb51d22ffc
Mila1308 <mpodrug1308@gmail.com>
2024-10-28 17:56:18 +0100

    Fix eriks merge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
-import Root from'./routes/Root'
+import App from './App.jsx'
 import './index.css'
-import ErrorPage from './ErrorPage'
-import Login from './routes/Login'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />,
-  }
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
