83fc42c5c1e963e2bf892bd764bd3ca1740a699b
Karlo <karlo.mezdic@fer.unizg.hr>
2024-10-30 18:52:57 +0100

    Add form submiting option to path /users/login, add new buttons for oauth login (not functional yet)

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import '../styles/Login.css'
 
+
 function Root() {
   const [count, setCount] = useState(0)
 
@@ -12,28 +13,62 @@ function Root() {
 }
 
 function LoginForm(){
-    return (
-        <>
-        <div className='formWrapper'>
-          <form className='forms'>
-            <div className='formDiv'> 
-              <h1 className='helloText'>Hello!</h1>
-              <div className='inputDiv'>
-                  <input className='infoInput' placeholder='Username'/>
-                  <input className='passwordInput' placeholder='Password'/>
-              </div>
-              <div className='buttonDiv'>
-                <button className='inputButton'>Sign In</button>
-              </div> 
-              <div className='redirect'>
-                <p className='account'>Dont have account?</p>
-                <a href='/register' className='link'>Register here</a>
-              </div>
-              
+
+	const [loginForm, setLoginForm] = useState({username:'', password:''});
+
+	function onChange(event){
+		const {name, value} = event.target;
+		setLoginForm(oldForm => ({...oldForm,[name]:value}));
+	}
+
+	function onSubmit(e){
+		e.preventDefault();
+
+		const data = {
+			username: loginForm.username,
+			password: loginForm.password,
+		};
+
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type' : 'application/json'
+			},
+			body: JSON.stringify(data)
+		};
+		
+		fetch('/users/login', options)
+	}
+
+  return (
+    <>
+    	<div className='formWrapper'>
+        <form className='forms' onSubmit={onSubmit}>
+          <div className='formDiv'> 
+            <h1 className='helloText'>Hello!</h1>
+            <div className='inputDiv'>
+                <input className='infoInput' placeholder='Username' name='username' onChange={onChange} value={loginForm.username}/>
+                <input className='passwordInput' placeholder='Password' name='password' onChange={onChange} type='password' value={loginForm.password}/>
             </div>
-          </form>
-        </div>
-        </>
-    )
+            <div className='buttonDiv'>
+              <button className='inputButton' type='submit'>Sign In</button>
+              </div> 
+            <div className='redirect'>
+              <p className='account'>Dont have account?</p>
+              <a href='/users/register' className='link'>Register here</a>
+            </div>
+						<div className='oauth'>
+							<p className='or'>OR</p>
+							<button className='oauthButton'>
+								<i className="fab fa-google icon"></i>  Use google account
+							</button>
+							<button className='oauthButton'>
+							<i className="fab fa-github icon"></i>  Use github account</button>
+						</div>
+          </div>
+        </form>
+      </div>
+      </>
+  )
 }
 export default Root
