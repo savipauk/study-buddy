69f4a48a02f0a9c5e172c60fc5b7f8de68a3a7dc
Karlo Mezdic <karlo.mezdic@fer.unizg.hr>
2024-10-30 20:18:27 +0100

    Dev/karlo (#19)
    * Add register view
    
    * Add login view
    
    * Change style of the app and add navigation
    
    * Add form submiting option to path /users/login, add new buttons for oauth login (not functional yet)
    
    * Add password validation in register form, submit option to path /users/register and change some visuals
    
    * remove node_module for fontawesome
    
    * fix bad indentatios
    
    * Add formatting config
    
    * remove unnecessary line
    
    * changeformatting config
    
    ---------
    
    Co-authored-by: Karlo <karlom@Ubuntu.myguest.virtualbox.org>

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,18 +1,90 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../styles/Root.css'
+import { useState } from "react";
+import "../styles/Login.css";
 
 function Root() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        Login
-      </div>
+      <LoginForm />
     </>
-  )
+  );
 }
 
-export default Root
+function LoginForm() {
+  const [loginForm, setLoginForm] = useState({ username: "", password: "" });
+
+  function onChange(event) {
+    const { name, value } = event.target;
+    setLoginForm((oldForm) => ({ ...oldForm, [name]: value }));
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+      username: loginForm.username,
+      password: loginForm.password,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch("/users/login", options);
+  }
+
+  return (
+    <>
+      <div className="formWrapper">
+        <form className="forms" onSubmit={onSubmit}>
+          <div className="formDiv">
+            <h1 className="helloText">Hello!</h1>
+            <div className="inputDiv">
+              <input
+                className="infoInput"
+                placeholder="Username"
+                name="username"
+                onChange={onChange}
+                value={loginForm.username}
+              />
+              <input
+                className="passwordInput"
+                placeholder="Password"
+                name="password"
+                onChange={onChange}
+                type="password"
+                value={loginForm.password}
+              />
+            </div>
+            <div className="buttonDiv">
+              <button className="inputButton" type="submit">
+                Sign In
+              </button>
+            </div>
+            <div className="redirect">
+              <p className="account">Dont have account?</p>
+              <a href="/users/register" className="link">
+                Register here
+              </a>
+            </div>
+            <div className="oauth">
+              <p className="or">OR</p>
+              <button className="oauthButton">
+                <i className="fab fa-google icon"></i> Use google account
+              </button>
+              <button className="oauthButton">
+                <i className="fab fa-github icon"></i> Use github account
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+export default Root;
