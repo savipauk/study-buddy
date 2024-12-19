9abcc190d95de780bdaa15614cae71d03588f6e2
Erik Kranjec <erik.kranjec8@outlook.com>
2024-12-19 15:08:07 +0100

    Mock security config

--- src/test/java/com/study_buddy/study_buddy/RestApiTest.java
+++ src/test/java/com/study_buddy/study_buddy/RestApiTest.java
@@ -2,6 +2,7 @@ package com.study_buddy.study_buddy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Set;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,10 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestApiTest {
@@ -19,7 +24,6 @@ class RestApiTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
-	@Test
 	void exampleRestApiCallReturnsHelloWorld() throws Exception {
 		Map<String, String> helloWorld = Map.of("hello", "world");
 		// No need for a DTO because this is not a complex object
@@ -33,11 +37,28 @@ class RestApiTest {
 	}
 
 	// TODO: Create a mock for security config
-	//@Test
-	//void nonExistingEndpointReturnsNotFound() throws Exception {
-	//	ResponseEntity<String> response = this.restTemplate.getForEntity("/nonExistingEndpoint", String.class);
-	//	assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-	//}
+	@Test
+	void nonExistingEndpointReturnsNotFound() throws Exception {
+		Map<String, Object> attributes = Map.of(
+				"sub", "123456",
+				"email", "authorizeduser@example.com");
+
+		DefaultOAuth2User mockUser = new DefaultOAuth2User(
+				Set.of(new OAuth2UserAuthority(attributes)),
+				attributes,
+				"email");
+
+		SecurityContextHolder.getContext()
+				.setAuthentication(new UsernamePasswordAuthenticationToken(mockUser, null, mockUser.getAuthorities()));
+
+		try {
+			ResponseEntity<String> response = this.restTemplate.getForEntity("/nonExistingEndpoint", String.class);
+			assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		} finally {
+			SecurityContextHolder.clearContext();
+		}
+
+	}
 
 	@Test
 	void unauthorizedEndpointReturnsUnauthorized() throws Exception {
