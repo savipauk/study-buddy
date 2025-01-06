6fb4a1e02cefb3007b6c2e6381a5d2f7a69c4acc
Ella Grković <ella.grkovic@fer.unizg.hr>
2025-01-06 20:55:27 +0100

    Design changes (#87)
    * Design changes
    
    * Design changes - part two
    
    * Design changes - unnecessary comments removed

--- frontend/src/routes/ProfileInfo.jsx
+++ frontend/src/routes/ProfileInfo.jsx
@@ -36,31 +36,33 @@ function ProfileInfo() {
   }, [isProfileSetupComplete, navigate]);
 
   return (
-    <div>
-      <Header />
-      <div className="profileInfoWrapper">
-        <div className="profilePictureInfo">
-          <img
-            src="https://static.vecteezy.com/system/resources/previews/005/129/844/non_2x/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg"
-            className="image"
-          ></img>
-        </div>
-        <div className="accountInfo">
-          <div className="username">
-            <label>{userInfoForm.FirstName}</label>
+      <div>
+        <Header />
+        <div className="profileWrapper">
+          <div className="profileInfoWrapper">
+            <div className="profilePictureInfo">
+              <img
+                  src="https://static.vecteezy.com/system/resources/previews/005/129/844/non_2x/profile-user-icon-isolated-on-white-background-eps10-free-vector.jpg"
+                  className="image"
+              ></img>
+            </div>
+            <div className="accountInfo">
+              <div className="username">
+                <label>{userInfoForm.FirstName}</label>
+              </div>
+              <div className="username">
+                <label>{userInfoForm.LastName}</label>
+              </div>
+            </div>
+            <button className="editProfileButton" onClick={handleProfileClick}>
+              <i className="fa-solid fa-user-pen"></i>
+            </button>
           </div>
-          <div className="username">
-            <label>{userInfoForm.LastName}</label>
+          <div className="groupHistory">
+            <label className="groupHistoryTitle">Popis STUDY GROUP-a</label>
           </div>
         </div>
-        <button className="editProfileButton" onClick={handleProfileClick}>
-          <i className="fa-solid fa-user-pen"></i>
-        </button>
       </div>
-      <div className="groupHistory">
-        <label className="groupHistoryTitle">StudyGroup history</label>
-      </div>
-    </div>
   );
 }
 export default ProfileInfo;
