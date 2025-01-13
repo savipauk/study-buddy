53fdc4d259d57e242a9bb3af0e7975b617e1da61
AnamarijaKic <anamarija.kic@fer.unizg.hr>
2025-01-13 17:54:53 +0100

    Change ResponseEntity for Report

--- src/main/java/com/study_buddy/study_buddy/service/ReportService.java
+++ src/main/java/com/study_buddy/study_buddy/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.study_buddy.study_buddy.service;
 
 
+import com.study_buddy.study_buddy.dto.ReportDto;
 import com.study_buddy.study_buddy.model.Report;
 import com.study_buddy.study_buddy.model.Status;
 import com.study_buddy.study_buddy.repository.ReportRepository;
@@ -22,4 +23,14 @@ public class ReportService {
     public List<Report> getAllReports() { return reportRepository.findAll();}
 
     public List<Report> getAllByReportStatus(Status status) { return reportRepository.findByStatus(status);}
+
+    public ReportDto convertToDto(Report report) {
+        ReportDto dto = new ReportDto();
+        dto.setReporterEmail(report.getReporter().getEmail());
+        dto.setReporterUsername(report.getReporter().getUsername());
+        dto.setReportedEmail(report.getReportedUser().getEmail());
+        dto.setReportedUsername(report.getReportedUser().getUsername());
+        dto.setReason(report.getReason());
+        return dto;
+    }
 }
