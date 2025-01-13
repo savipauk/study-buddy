7888c665505c08c11a7b1b826a7d2c94792a780d
AnamarijaKic <anamarija.kic@fer.unizg.hr>
2025-01-13 14:44:22 +0100

    Filter StudyGroups

--- src/main/java/com/study_buddy/study_buddy/repository/StudyGroupRepository.java
+++ src/main/java/com/study_buddy/study_buddy/repository/StudyGroupRepository.java
@@ -3,6 +3,8 @@ package com.study_buddy.study_buddy.repository;
 import com.study_buddy.study_buddy.model.Student;
 import com.study_buddy.study_buddy.model.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,11 @@ public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long> {
     StudyGroup findByGroupId(Long group_id);
     List<StudyGroup> findByCreator_StudentId(Long creator_id);
     List<StudyGroup> findByCreator(Student creator);
+
+    // Queries for filtration
+    // 1) check the groupName 2) check the location 3) check the description
+    @Query("SELECT s FROM StudyGroup s WHERE LOWER(s.groupName) LIKE LOWER(CONCAT('%', :name, '%')) " +
+            "OR LOWER(s.location) LIKE LOWER(CONCAT('%', :name, '%')) " +
+            "OR LOWER(s.description) LIKE LOWER(CONCAT('%', :name, '%')) ")
+    List<StudyGroup> findByGroupNameOrLocationOrDescriptionIgnoreCase(@Param("name") String name);
 }
