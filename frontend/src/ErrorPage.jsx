cd8cc0468a0b32e5b52daa5c9193480c9b0f28ba
Ella Grkovic <ella.grkovic@fer.unizg.hr>
2025-01-14 16:57:23 +0100

    language change

--- frontend/src/ErrorPage.jsx
+++ frontend/src/ErrorPage.jsx
@@ -1,13 +1,14 @@
 import { useRouteError } from 'react-router-dom';
 import './styles/Root.css';
+
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
     <div id="error-page">
-      <h1 className="er">Oops!</h1>
-      <p className="errorText">Sorry, an unexpected error has occurred.</p>
+      <h1 className="er">Ups!</h1>
+      <p className="errorText">Oprostite, dogodila se neočekivana pogreška.</p>
       <p className="errorText">
         <i>{error.statusText || error.message}</i>
       </p>
