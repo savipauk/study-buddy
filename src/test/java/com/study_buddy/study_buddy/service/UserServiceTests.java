2a53e038ce48eb728dcfd72dca966e4ce8864c01
AnamarijaKic <anamarija.kic@fer.unizg.hr>
2025-01-16 00:19:53 +0100

    Test UserService getUserByEmail function

--- src/test/java/com/study_buddy/study_buddy/service/UserServiceTests.java
+++ src/test/java/com/study_buddy/study_buddy/service/UserServiceTests.java
@@ -4,6 +4,7 @@ import com.study_buddy.study_buddy.model.Gender;
 import com.study_buddy.study_buddy.model.StudyRole;
 import com.study_buddy.study_buddy.model.User;
 import com.study_buddy.study_buddy.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,9 +29,11 @@ public class UserServiceTests {
     @InjectMocks
     private UserService userService;
 
-    @Test
-    public void UserService_CreateUser_ReturnsUser(){
-        User user = new User();
+    private User user;
+
+    @BeforeEach
+    public void init(){
+        user = new User();
         user.setUserId(1L);
         user.setEmail("test@example.com");
         user.setPassword("password123");
@@ -46,10 +49,25 @@ public class UserServiceTests {
         user.setCity("New York");
         user.setOauthProvider("");
         user.setOauthId("");
+    }
 
+
+    @Test
+    public void UserService_GetUserByEmail_ReturnsUser(){
+        when(userRepository.findByEmail(Mockito.any(String.class))).thenReturn(user);
+
+        User foundUser = userService.getUserByEmail(user.getEmail());
+
+        assertNotNull(foundUser);
+        assertEquals(user.getEmail(),foundUser.getEmail());
+    }
+
+    @Test
+    public void UserService_CreateUser_ReturnsUser(){
         when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
 
         User savedUser = userService.createUser(user);
+
         assertNotNull(savedUser);
         assertEquals(user.getEmail(),savedUser.getEmail());
     }
